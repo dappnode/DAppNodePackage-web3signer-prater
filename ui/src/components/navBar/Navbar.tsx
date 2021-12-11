74d4ac4c0a92a5e51eaa9d3f859dcf1a03748490
pablo <mendez4a@gmail.com>
2021-12-11 02:46:56 +0100

    responsive nav bar

--- ui/src/components/navBar/Navbar.tsx
+++ ui/src/components/navBar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { navBarData } from "./navBarData";
 // import styles
@@ -7,16 +7,24 @@ import NavBarTitle from "./NavBarTitle";
 import NavBarMedia from "./NavBarMedia";
 
 export default function NavigationBar() {
+  const [screenWidth, setScreenWidth] = useState(window.screen.width);
+
+  useEffect(() => {
+    const handleResize = () => setScreenWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [screenWidth]);
+
   return (
     <>
       <NavBarTitle />
-      <hr />
+      <hr className="nav-bar-separator" />
       <ul className="nav-bar-items">
         {navBarData.map((item, index) => (
           <li key={index} className={item.cname}>
             <Link to={item.path}>
               {<item.icon />}
-              <span>{item.title}</span>
+              {screenWidth > 480 && <span>{item.title}</span>}
             </Link>
           </li>
         ))}
