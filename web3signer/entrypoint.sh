9c9339835323a60a9bbfc454729293d6fad582ef
pablo <mendez4a@gmail.com>
2022-01-21 15:47:07 +0100

    build from source

--- web3signer/entrypoint.sh
+++ web3signer/entrypoint.sh
@@ -94,4 +94,4 @@ fi
 rm -rf $KEYFILES_DIR_TMP/*
 
 # Run web3signer binary
-exec /opt/web3signer/bin/web3signer --key-store-path="$KEYFILES_DIR" --http-listen-port=9003 --http-listen-host=0.0.0.0 --http-host-allowlist=* eth2 --network=prater --slashing-protection-db-url=jdbc:postgresql://postgres:5432/web3signer --slashing-protection-db-username=postgres --slashing-protection-db-password=password
+exec /opt/web3signer/bin/web3signer --key-store-path="$KEYFILES_DIR" --http-listen-port=9003 --http-listen-host=0.0.0.0 --http-host-allowlist=* eth2 --network=prater --slashing-protection-db-url=jdbc:postgresql://postgres:5432/web3signer --slashing-protection-db-username=postgres --slashing-protection-db-password=password --key-manager-api-enabled=true
