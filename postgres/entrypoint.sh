dedebd9c6c1283a2c0c667c186cb39ddc0cf7f5c
pablomendezroyo <mendez4a@gmail.com>
2023-03-21 11:39:39 +0100

    Add flyway service

--- postgres/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-for file in /docker-entrypoint-initdb.d/*.sql
-do
-  echo "Executing $file"
-  psql --host=localhost --port=5432 --dbname=${POSTGRES_DB} --username=${POSTGRES_USER} -f "$file"
-done
-
-exec "postgres"
