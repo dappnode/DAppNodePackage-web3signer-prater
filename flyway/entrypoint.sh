1aa53cc602fe878da1d1772f79873a6c8075a821
pablomendezroyo <mendez4a@gmail.com>
2023-03-22 12:40:46 +0100

    Add checker for integers

--- flyway/entrypoint.sh
+++ flyway/entrypoint.sh
@@ -6,6 +6,12 @@ DATABASE_VERSION=$(PGPASSWORD=password psql --tuples-only -U postgres -h postgre
 # Get the latest migration file version
 LATEST_MIGRATION_VERSION=$(ls -1 /flyway/sql/ | tail -n 1 | cut -d'_' -f1 | cut -d'V' -f2 | sed 's/^0*//')
 
+# Ensure that either DATABASE_VERSION and LATEST_MIGRATION_VERSION are integers
+if ! [[ "$DATABASE_VERSION" =~ ^[0-9]+$ ]] || ! [[ "$LATEST_MIGRATION_VERSION" =~ ^[0-9]+$ ]]; then
+  echo "ERROR: could not compare database and latest migration file versions. Exiting WITHOUT DATABASE MIGRATIONS. This may result in unexpected behaviour"
+  exit 0
+fi
+
 if [ "$DATABASE_VERSION" -ge "$LATEST_MIGRATION_VERSION" ]; then
   echo "Database version is greater or equal to the latest migration file version. Exiting..."
   exit 0
